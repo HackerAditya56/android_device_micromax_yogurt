f1cce45e2f3580b85763e6f6155b069e11780709
ADITYA NANDAN <adityanandan137@gmail.com>
2025-05-26 16:03:36 +0530

    fix bootloop becaus of the wrong rc

--- device.mk
+++ device.mk
@@ -234,13 +234,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.yogurt
     
-DEVICE_MANIFEST_FRAGMENT += \
-     $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.yogurt.xml
-     
-# Add Fingerprint HAL RC file
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.yogurt.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.yogurt.rc
-    
 # KPOC
 PRODUCT_PACKAGES += \
     libsuspend
