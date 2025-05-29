b683d0fedeb8b628917beb2fb1266c05fd529131
ADITYA NANDAN <adityanandan137@gmail.com>
2025-05-29 14:06:57 +0530

    changes for derp build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_yogurt.mk
+    $(LOCAL_DIR)/derp_yogurt.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_yogurt-user \
-    aosp_yogurt-userdebug \
-    aosp_yogurt-eng
+    derp_yogurt-user \
+    derp_yogurt-userdebug \
+    derp_yogurt-eng
